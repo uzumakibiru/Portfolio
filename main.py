5d885d5b521db323ce3376b0f0cebc9cba759ad2
Birochan Chaudhary <you@example.combirohzyan@gmail.comgit config --global user.name Your>
2024-07-10 20:34:06 +1000

    Version 1.1 with slight Graphical changes

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from flask import Flask, request,render_template
 from send import SendMessage
+from random import choice
 
-
+img_list=[f"Nemo ({i+1})" for i in range(23)]
 app= Flask(__name__)
+
+def random_logo():
+    img=choice(img_list)
+    return img
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    logo_img=random_logo()
+    return render_template("index.html",logo_img=logo_img)
 
 @app.route("/contact",methods=["GET","POST"])
 def contact():
+    logo_img=random_logo()
     if request.method=="GET":
-        return render_template("contact.html")
+        return render_template("contact.html",logo_img=logo_img)
     else:
         name=request.form["name"]
         email=request.form["email"]
@@ -19,7 +26,7 @@ def contact():
         msg=SendMessage(name,email,message)
         msg.from_sender()
         msg.to_sender()
-        return render_template("contact.html")
+        return render_template("contact.html",logo_img=logo_img)
 
 if __name__=="__main__":
     app.run(debug=True)
