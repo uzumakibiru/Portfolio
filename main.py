84b31bf3b4597cbfec5194d31c9d3c9fe2f92081
Birochan Chaudhary <you@example.combirohzyan@gmail.comgit config --global user.name Your>
2024-07-10 21:03:17 +1000

    Minor changes on footer

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request,render_template
 from send import SendMessage
 from random import choice
+from datetime import datetime 
+
+current_time=datetime.now()
+year =current_time.year
 
 img_list=[f"Nemo ({i+1})" for i in range(23)]
 app= Flask(__name__)
@@ -12,13 +16,13 @@ def random_logo():
 @app.route("/")
 def home():
     logo_img=random_logo()
-    return render_template("index.html",logo_img=logo_img)
+    return render_template("index.html",logo_img=logo_img,year=year)
 
 @app.route("/contact",methods=["GET","POST"])
 def contact():
     logo_img=random_logo()
     if request.method=="GET":
-        return render_template("contact.html",logo_img=logo_img)
+        return render_template("contact.html",logo_img=logo_img,year=year)
     else:
         name=request.form["name"]
         email=request.form["email"]
@@ -26,7 +30,7 @@ def contact():
         msg=SendMessage(name,email,message)
         msg.from_sender()
         msg.to_sender()
-        return render_template("contact.html",logo_img=logo_img)
+        return render_template("contact.html",logo_img=logo_img,year=year)
 
 if __name__=="__main__":
     app.run(debug=True)
